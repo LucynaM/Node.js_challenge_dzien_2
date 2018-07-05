aacff6dc6aeebc8437365791f32ec394d384705d
Lucyna Mazur <lucyna@studiokreator.com>
2018-07-05 22:14:36 +0200

    second day

--- app/zadanie01.js
+++ app/zadanie01.js
@@ -1,1 +1,23 @@
 //Twój kod
+
+const fs = require('fs');
+
+fs.readFile('./data/zadanie01/input.json', 'utf8', (err, data) => {
+	if (err===null) {
+		const dataConverted = JSON.parse(data);
+		let sumData = 0;
+		for (let i = 0; i < dataConverted.length; i++) {
+			sumData += dataConverted[i]
+		}
+
+		fs.writeFile('./data/zadanie01/sum.txt', sumData, err => {
+			if (err===null) {
+				console.log('Zapisano poprawnie!');
+			} else {
+				console.log('Błąd podczas zapisu pliku', err)
+			}
+		})
+	} else {
+		console.log('błąd podczas odczytu pliku', err)
+	}
+});
--- app/zadanieDnia.js
+++ app/zadanieDnia.js
@@ -1,1 +1,37 @@
 //Twój kod
+
+const fs = require('fs');
+const file = process.argv[2];
+
+console.log(process.argv[2]);
+
+fs.readFile(file, 'utf8', (err, data) => {
+	if (err===null) {
+		
+		console.log('Poprawnie odczytano plik: ' + file);
+		
+		let newData = '';
+		let j = 0;
+		for (let i = 0; i < data.length; i++) {
+			if (/[a-zA-Z]/.test(data[i])) {
+				if (j % 2 === 0) {
+					newData += data[i].toUpperCase();
+				} else {
+					newData += data[i].toLowerCase();
+				}
+				j++;
+			} else {
+				newData += data[i];
+			}
+		}
+		fs.writeFile(file, newData, err => {
+			if (err===null) {
+				console.log('Zapisano poprawnie!');
+			} else {
+				console.log('Błąd podczas zapisu pliku', err)
+			}
+		})
+	} else {
+		console.log('Błąd podczas odczytu pliku!', err);
+	}
+});
